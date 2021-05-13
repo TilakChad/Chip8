e46fbf05e003acdea60be22be2367e2a8cef7674
tilakchad111 <tilakchad111@gmail.com>
2021-05-13 23:03:40 +0545

    added rom loading and tick to handle delay

--- includes/chip8.h
+++ includes/chip8.h
@@ -5,6 +5,7 @@
 #include "renderer.h"
 #include <stdio.h>
 #include <stdbool.h>
+#include <GLFW/glfw3.h>
 
 typedef struct chip8_emulator
 {
@@ -16,13 +17,18 @@ typedef struct chip8_emulator
 	uint8_t delay_register;
 	uint16_t stack_pointer; 
 	uint16_t current_instruction;
-
+	uint8_t recent_key;
+	bool should_render;
+	float time_accumulate;
 } chip8_emulator;
 
-void initialize_chip8_emulator(chip8_emulator* chip8);
+void initialize_chip8_emulator(chip8_emulator* chip8, const char* rom_path);
 void write_sprite_data(chip8_emulator*);
 void render_sprite(chip8_emulator*,struct frameBuffer*, int, int,int);
 
 void fetch_instruction(chip8_emulator*);
-void decode_and_execute(chip8_emulator*, struct frameBuffer* );
+void decode_and_execute(chip8_emulator*, struct frameBuffer*, GLFWwindow* );
+void load_rom(chip8_emulator*, const char* rom_location);
+void tick(chip8_emulator*, float deltaTime);
+
 #endif
